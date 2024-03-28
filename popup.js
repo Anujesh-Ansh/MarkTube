e30cf500c3efcf01289f551002a1ecfa6ffdf7a1
Anujesh Ansh <anshanujesh@gmail.com>
2024-03-28 21:59:49 +0530

    making Changes

--- popup.js
+++ popup.js
@@ -1,7 +1,28 @@
-// adding a new bookmark row to the popup
-const addNewBookmark = () => {};
+import {getActiveTabURL} from "./utils.js";
+const addNewBookmark = (bookmarksElement, bookmark) => {
+    const bookmarkTitleElement = document.createElement("div");
+    const newBookmarkElement = document.createElement("div");
 
-const viewBookmarks = () => {};
+    bookmarkTitleElement.textContent = bookmark.desc;
+    bookmarkTitleElement.className = "bookmark-title";
+
+    
+};
+
+const viewBookmarks = (currentVideoBookmarks = []) => {
+    const bookmarksElement = document.getElementById("bookmarks");
+    bookmarksElement.innerHTML = "";
+
+    if (currentVideoBookmarks.length > 0) {
+        for (let i = 0; i < currentVideoBookmarks.length; i++) {
+            const bookmark = currentVideoBookmarks[i];
+            addNewBookmark(bookmarksElement, bookmark);
+        }
+    } else {
+        bookmarksElement.innerHTML = '<i class="row">No bookmarks found!</i>';
+    }
+};
+
 
 const onPlay = e => {};
 
@@ -9,4 +30,22 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+    const activeTab = await getActiveTabURL();
+    const queryParameters = activeTab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    const currentVideo = urlParameters.get("v");
+
+    if(activeTab.url.includes("youtube.com/watch") && currentVideo) {
+        chrome.storage.sync.get([currentVideo], (data) => {
+            const currentVideoBookmarks = data[currentVideo] ? JSON.parse(data[currentVideo]) : [];
+
+            viewBookmarks(currentVideoBookmarks);
+
+        });
+    } else{
+        const container = document.getElementsByClassName("container")[0];
+        container.innerHTML = '<div class="title">Open a YouTube video to start bookmarking!</div>';
+    }
+});
